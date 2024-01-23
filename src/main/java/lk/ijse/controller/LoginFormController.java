bfca066819327bd2988bf0136700f12083c56290
ThesanduJ <thesandu1602500@gmail.com>
2024-01-23 17:40:25 +0530

    finished login page

--- src/main/java/lk/ijse/controller/LoginFormController.java
+++ src/main/java/lk/ijse/controller/LoginFormController.java
@@ -3,7 +3,17 @@ package lk.ijse.controller;
 import com.jfoenix.controls.JFXButton;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import lk.ijse.Launcher;
+
+import java.io.IOException;
 
 public class LoginFormController {
     @FXML
@@ -13,7 +23,21 @@ public class LoginFormController {
     private TextField userName;
 
     @FXML
-    void btnLoginOnAction(ActionEvent event) {
+    void btnLoginOnAction(ActionEvent event) throws Exception {
+        String title = (String) userName.getText();
+        if (!title.equals("")) {
 
+            Parent root = FXMLLoader.load(getClass().getResource("/view/chat_form.fxml"));
+            Scene scene = new Scene(root);
+            Stage stage = new Stage();
+            stage.setTitle(title + "'s Chat Room");
+            stage.setScene(scene);
+            Image image=new Image("/assets/chat Icon.png");
+            stage.getIcons().add(image);
+            stage.setResizable(false);
+            stage.show();
+        } else {
+            new Alert(Alert.AlertType.ERROR, "Can not continue this chat application without username").show();
+        }
     }
 }
