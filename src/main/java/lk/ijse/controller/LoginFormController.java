ed31f0ec99c6cbce5f9a71be9dcb59cf602fab28
ThesanduJ <thesandu1602500@gmail.com>
2024-01-23 14:20:37 +0530

    Create login page

---
+++ src/main/java/lk/ijse/controller/LoginFormController.java
@@ -0,0 +1,19 @@
+package lk.ijse.controller;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class LoginFormController {
+    @FXML
+    private JFXButton loginButton;
+
+    @FXML
+    private TextField userName;
+
+    @FXML
+    void btnLoginOnAction(ActionEvent event) {
+
+    }
+}
