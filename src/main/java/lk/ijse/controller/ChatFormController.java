e00b6f6b2e692e329d3058332599f09a7c7a1542
ThesanduJ <thesandu1602500@gmail.com>
2024-02-01 19:33:12 +0530

    change the UI design

--- src/main/java/lk/ijse/controller/ChatFormController.java
+++ src/main/java/lk/ijse/controller/ChatFormController.java
@@ -1,5 +1,44 @@
 package lk.ijse.controller;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+
 public class ChatFormController {
+
+    @FXML
+    private JFXButton btnFile;
+
+    @FXML
+    private JFXButton btnSend;
+
+    @FXML
+    private AnchorPane rootNode2;
+
+    @FXML
+    private ScrollPane scrollPane;
+
+    @FXML
+    private VBox vbox;
+    @FXML
+    private JFXButton btnEmoji;
+
+
+    @FXML
+    void btnFIleOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSendOnAction(ActionEvent event) {
+
+    }
+    @FXML
+    void btnEmojiOnAction(ActionEvent event) {
+
+    }
 
 }
